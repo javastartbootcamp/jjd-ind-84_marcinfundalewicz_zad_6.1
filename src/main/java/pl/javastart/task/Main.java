fe37220b8ba619810b0ad472f9cbe7b98fa690db
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-17 15:05:28 +0100

    commit 1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,30 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe");
+        int number = scanner.nextInt();
+        int min = 100;
+        int max = 200;
+        while (number % 3 != 0) {
+            if (number > max) {
+                System.out.println("Liczba jest za duza");
+                System.out.println("Podaj liczbe");
+                number = scanner.nextInt();
+            } else if (number < min) {
+                System.out.println("Liczba jest za mala");
+                System.out.println("Podaj liczbe");
+                number = scanner.nextInt();
+            } else if (number % 3 != 0) {
+                System.out.println("Liczba niejest podzielna przez 3");
+                System.out.println("Podaj liczbe");
+                number = scanner.nextInt();
+            }
+        }
+        System.out.println("Twoja liczba jest ok");
     }
 }
